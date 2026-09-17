7638acddd82bdb00622b7608cde1fb29f5823e0e
Jay kishan <jkn18869@gmail.com>
2026-09-17 23:52:31 +0530

    Time: 0 ms (100%), Space: 54 MB (27.32%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,11 @@
+/**58. Length of Last Word
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    
+    let words = s.trim().split(" ");
+
+    return words[words.length - 1].length;
+  
+};
