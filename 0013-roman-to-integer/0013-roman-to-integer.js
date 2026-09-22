5c1036a207dff1a45048fa0e39589c877952c969
Jay kishan <jkn18869@gmail.com>
2026-09-22 23:54:21 +0530

    Time: 9 ms (18.92%), Space: 61.2 MB (45.11%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,29 @@
+/*
+*13. Roman to Integer
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanMap = {
+'I': 1,
+'V': 5,
+'X':10,
+'L': 50,
+'C':100,
+'D': 500,
+'M': 1000 }
+    let sum = 0;
+    for(let i = 0; i < s.length; i++){
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i+1]];
+        if(next > current){
+            sum += (next - current);
+            i++;
+        }
+        else{
+            sum += current;
+        }
+    }
+    return sum;
+    
+};
